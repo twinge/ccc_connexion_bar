337a013cbe4f6002a6c227b3c9d2e3df7b0d473d
Andrew Roth <andrewroth@gmail.com>
2009-02-19 17:57:45 -0500

    typo on connexion_defaults.css (should be connexion_default.css)

--- init.rb
+++ init.rb
@@ -2,8 +2,8 @@ require 'connexion_bar'
 require 'ftools'
 
 # copy our connexion.css to public if it's not there
-src = File.join(File.dirname(__FILE__), 'assets', 'connexion_defaults.css')
-dest = File.join(RAILS_ROOT, 'public', 'stylesheets', 'connexion_defaults.css')
+src = File.join(File.dirname(__FILE__), 'assets', 'connexion_default.css')
+dest = File.join(RAILS_ROOT, 'public', 'stylesheets', 'connexion_default.css')
 
 unless File.exist? dest
   File.copy src, dest
